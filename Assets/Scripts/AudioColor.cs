6288bb212448293c68664b7313090223a23345d7
ElaGough <ela.gough@gmail.com>
2018-12-20 03:32:13 +0000

    remove unneccessary code

--- Assets/Scripts/AudioColor.cs
+++ Assets/Scripts/AudioColor.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//used for Color.lerp. Needed to get Color from Image Script
+//[RequireComponent(typeof(Image))]
+
 public class AudioColor : AudioTriggerSpectrum
 {
     /*//Renderer rend;
@@ -19,53 +22,16 @@ public class AudioColor : AudioTriggerSpectrum
         if (_isBeat)
         {
             float lerp = Mathf.PingPong(Time.time, duration) / duration;
-            this.GetComponent<Renderer>().material.color = Color.Lerp(restColor, beatColor, lerp);
+            this.GetComponent<Image>().color = Color.Lerp(restColor, beatColor, lerp);
         }
         else if (!_isBeat)
         {
             float lerp = Mathf.PingPong(Time.time, duration) / duration;
-            this.GetComponent<Renderer>().material.color = Color.Lerp(beatColor, restColor, lerp);
+            this.GetComponent<Image>().color = Color.Lerp(beatColor, restColor, lerp);
         }
 
     }*/
 
 
-    /*
-    public Color startColor;
-    public Color endColor;
-
-    public float fadeInTime = 1f;
-    public float fadeOutTime = 2f;
-    public float fadeDelay = 3f;
-
-    public SpriteRenderer _spriteRender;
-
-    void Awake()
-    {
-        _spriteRender = this.GetComponent<SpriteRenderer>();
-    }
-
-    void Start()
-    {
-        StartCoroutine(colorLerpIn());
-    }
-
-    IEnumerator colorLerpIn()
-    {
-        for(float t = 0.01f; t < fadeInTime; t += 0.1f)
-        {
-            _spriteRender.material.color = Color.Lerp(startColor, endColor, t / fadeInTime);
-            yield return null;
-        }
-        StartCoroutine(colorLerpOut());
-    }
-
-    IEnumerator colorLerpOut()
-    {
-        for (float t = 0.01f; t < fadeOutTime; t += 0.1f)
-        {
-            _spriteRender.material.color = Color.Lerp(startColor, endColor, t / fadeOutTime);
-            yield return null;
-        }
-    }*/
+   
 }
